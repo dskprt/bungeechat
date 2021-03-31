a82796d981f4086317b51bac103ea59703e07cba
dskprt <59660013+dskprt@users.noreply.github.com>
2021-03-31 09:39:30 +0200

    project files

---
+++ proxy/src/main/java/com/github/dskprt/bungeechat/Bungeechat.java
@@ -0,0 +1,16 @@
+package com.github.dskprt.bungeechat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Bungeechat extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ spigot/src/main/java/com/github/dskprt/bungeechat/Bungeechat.java
@@ -0,0 +1,17 @@
+package com.github.dskprt.bungeechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bungeechat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
